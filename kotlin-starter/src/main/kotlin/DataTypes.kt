baaf8f26d7f43af825dea3dc060058fae946654f
riopitra2312 <example@mail.com>
2020-08-07 21:50:15 +0700

    first commit

---
+++ kotlin-starter/src/main/kotlin/DataTypes.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val valueA: Int = 43
+    val valueB = 2
+
+    val firstWord = "SMK "
+    val middleWord = "Telkom "
+    val lastWord = "Purwokerto "
+    val lastWord1 = "Luas Persegi Panjang "
+
+    println(firstWord + middleWord + lastWord)
+    print(lastWord1 + valueA * valueB)
+}
---
+++ kotlin-starter/src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Rios"
+    print("Nama Saya ")
+    println(name)
+    val hobi = "Main Game"
+    print("Hobi saya ")
+    println(hobi)
+
+}
